7b51f46504729b3bc6dd09c43252fad026364a4e
michaloumen <michelle.mendonca@minu.co>
2024-08-02 21:45:15 -0300

    accessing request data with decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
+// o controller é um class decorator porque está sendo aplicado a uma classe
 export class MessagesController {
   @Get()
+  // decorator de método
   listMessages() {
 
   }
-  
-  @Post()
-  createMessage() {
 
+  @Post()
+  createMessage(@Body() body: any) {
+    // body para extrair dados do corpo da requisição http
+    console.log(body);
   }
 
   @Get('/:id')
-  getMessage() {
-
+  getMessage(@Param('id') id: string) {
+    // param para extrair dado da URL
+    console.log(id);
   }
 }
